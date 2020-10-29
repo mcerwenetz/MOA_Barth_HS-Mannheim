a3d8d39df47c45a748d1cab2bbe14974497b0479
Marius Cerwenetz <cerwenetz.marius@gmail.com>
2020-10-29 18:59:59 +0100

    Unnötige Imports entfernt

--- ToggleApp/app/src/main/java/de/mc/toggleapp/CheckBoxActivity.java
+++ ToggleApp/app/src/main/java/de/mc/toggleapp/CheckBoxActivity.java
@@ -2,13 +2,10 @@ package de.mc.toggleapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
