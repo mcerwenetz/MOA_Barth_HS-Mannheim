8ce1e5675eb24d1f30e78dbfbb967292f85df160
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-11-01 15:41:55 +0100

    TableLayout implementiert

---
+++ Layouts/app/src/main/java/de/mc/layouts/TableLayout.java
@@ -0,0 +1,14 @@
+package de.mc.layouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class TableLayout extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tablelayout);
+    }
+}
