283c60d373e105f15bab5e48e975a4f092012733
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-11-01 16:32:15 +0100

    RelativeLayout implementiert

---
+++ Layouts/app/src/main/java/de/mc/layouts/RelativeLayout.java
@@ -0,0 +1,15 @@
+package de.mc.layouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class RelativeLayout extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relativelayout);
+    }
+}
