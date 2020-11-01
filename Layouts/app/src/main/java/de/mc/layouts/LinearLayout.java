a9f06bcdb6d11c8c4d760f9799f6d2b68bdba41e
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-11-01 15:25:58 +0100

    LinearLayout implementiert

---
+++ Layouts/app/src/main/java/de/mc/layouts/LinearLayout.java
@@ -0,0 +1,15 @@
+package de.mc.layouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class LinearLayout extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linearlayout);
+    }
+}
