8b7bf5575f22431236dfa0241df0a68fae12dbcf
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-11-11 11:14:27 +0100

    ArrayAdapter und onItemSelectedListener implementiert

--- MoneyConverter/app/src/main/java/de/mc/moneyconverter/MainActivity.java
+++ MoneyConverter/app/src/main/java/de/mc/moneyconverter/MainActivity.java
@@ -6,9 +6,10 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -23,10 +24,15 @@ public class MainActivity extends Activity{
 
     private EditText etAmount;
     private TextView tvResult;
+    private Spinner quellwaehrungsspinner;
+    private Spinner zielwaehrungsspinner;
     private String quellwaehrung;
     private String zielwaehrung;
     private BigDecimal amount;
     private BigDecimal result;
+
+    public MainActivity() {
+    }
 
 
     @Override
@@ -35,7 +41,43 @@ public class MainActivity extends Activity{
         setContentView(R.layout.moneyconverter);
         etAmount = findViewById(R.id.etAmountSource);
         tvResult = findViewById(R.id.tvResult);
+        quellwaehrungsspinner = findViewById(R.id.spinnerquellwaehrung);
+        zielwaehrungsspinner = findViewById(R.id.spinnerqzielwaehrung);
+
         tvResult.setText("0");
+        ArrayAdapter<CharSequence> waehrungsnamenadapter = ArrayAdapter.createFromResource(this, R.array.wechselkursNamen, android.R.layout.simple_spinner_item);
+        waehrungsnamenadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        AdapterView.OnItemSelectedListener quellOisl = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                quellwaehrung = (String) parent.getSelectedItem();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+        AdapterView.OnItemSelectedListener zielOisl = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                zielwaehrung = (String) parent.getSelectedItem();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+
+
+
+        quellwaehrungsspinner.setAdapter(waehrungsnamenadapter);
+        quellwaehrungsspinner.setAdapter(waehrungsnamenadapter);
+
+        quellwaehrungsspinner.setOnItemSelectedListener(quellOisl);
+        zielwaehrungsspinner.setOnItemSelectedListener(zielOisl);
 
         etAmount.addTextChangedListener(new TextWatcher() {
             @Override
