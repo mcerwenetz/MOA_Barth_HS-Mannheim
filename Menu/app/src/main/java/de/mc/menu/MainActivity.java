821a7c12df92e8236311d7d8e9a1c063379a244e
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-11-22 20:46:58 +0100

    Menu App implementiert

--- Menu/app/src/main/java/de/mc/menu/MainActivity.java
+++ Menu/app/src/main/java/de/mc/menu/MainActivity.java
@@ -1,24 +1,51 @@
 package de.mc.menu;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activitymain);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater= getMenuInflater();
-        inflater.inflate(R.menu.actions, menu);
-        return super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.item1){
+            finish();
+            return true;
+        } else if (item.getItemId() == R.id.item2){
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem searchItem = menu.findItem(R.id.menusearch);
+        if (searchItem.isEnabled()){
+            searchItem.setEnabled(false);
+        } else {
+            searchItem.setEnabled(true);
+        }
+        return super.onPrepareOptionsMenu(menu);
     }
 }
---
+++ Menu/app/src/main/java/de/mc/menu/SettingsActivity.java
@@ -0,0 +1,15 @@
+package de.mc.menu;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setContentView(R.layout.settings);
+        super.onCreate(savedInstanceState);
+
+    }
+}
