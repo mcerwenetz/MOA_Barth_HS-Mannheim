feb7d649e8b2c37803377dbfe1dbf3bb6fd6fdb7
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-11-23 16:09:33 +0100

    Preferences App hinzugefügt

--- Menu/app/src/main/java/de/mc/menu/MainActivity.java
+++ Menu/app/src/main/java/de/mc/menu/MainActivity.java
@@ -3,9 +3,14 @@ package de.mc.menu;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,10 +18,63 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MenuApp";
+
+
+
+    private ActionMode am = null;
+
+    ActionMode.Callback actionModeCallback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.contextactions, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            return handleMenuSelectedItem(item);
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            MainActivity.this.am = null;
+        }
+    };
+
+    private boolean handleMenuSelectedItem(MenuItem item) {
+        int position = pairsAdapter.getSelectedPosition();
+        if(position == -1){
+            return super.onContextItemSelected(item);
+        }
+        if(item.getItemId() == R.id.caItem1){
+
+        }
+    }
+
+    AdapterView.OnItemLongClickListener oilcl = new AdapterView.OnItemLongClickListener() {
+        @Override
+        public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+            Log.v(TAG, "long clicked on ActionBar");
+            if (am != null)
+                return  false;
+            am = MainActivity.this.startActionMode(actionModeCallback);
+            pairsAdapter.setSelectedPosition(position);
+            return true;
+        }
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitymain);
+
     }
 
     @Override
---
+++ Menu/app/src/main/java/de/mc/menu/PairsAdapter.java
@@ -0,0 +1,8 @@
+package de.mc.menu;
+
+import android.widget.ArrayAdapter;
+
+import java.lang.reflect.Array;
+
+public class PairsAdapter extends ArrayAdapter<Pair<String, String>> {
+}
