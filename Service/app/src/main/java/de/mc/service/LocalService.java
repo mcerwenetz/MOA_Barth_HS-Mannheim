f66c275f8e7359422f0d6313048ccada6df34d1e
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-12-20 13:50:54 +0100

    LocalService implementiert

--- Service/app/src/main/java/de/mc/service/LocalService.java
+++ Service/app/src/main/java/de/mc/service/LocalService.java
@@ -1,5 +1,35 @@
 package de.mc.service;
 
-public class LocalService {
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
 
+import androidx.annotation.Nullable;
+
+public class LocalService extends Service {
+
+    private IBinder binder = new LocalBinder();
+    public class LocalBinder extends Binder{
+        public LocalService getService(){
+            return LocalService.this;
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    public Integer doAdd(Integer zahl1, Integer zahl2){
+        int ret =0;
+        if (zahl1 != null){
+            ret += zahl1;
+        }
+        if (zahl2 != null){
+            ret +=zahl2;
+        }
+        return ret;
+    }
 }
